55abd7663d8b301e873975adde2f9f9d1fb6fa17
Mohamed KARIM <karimkar999@gmail.com>
2023-01-28 15:27:00 +0100

    add the implementation of std::equal

--- utils/lexicographical_compare.hpp
+++ utils/lexicographical_compare.hpp
@@ -6,7 +6,7 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 15:54:08 by mkarim            #+#    #+#             */
-/*   Updated: 2023/01/27 16:01:40 by mkarim           ###   ########.fr       */
+/*   Updated: 2023/01/28 15:24:48 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 #define LEXICOGRAPHICAL_HPP
 
 template <class InputIterator1, class InputIterator2>
-bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
-    InputIterator2 first2, InputIterator2 last2)
+bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
     {
         while (first1 != last1)
         {
@@ -30,19 +29,44 @@ bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
     }
 
 template <class InputIterator1, class InputIterator2>
-bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
-    InputIterator2 first2, InputIterator2 last2, Compare comp)
+bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
+{
+    while (first1 != last1)
     {
-        while (first1 != last1)
-        {
-            if (first2 == last2 || comp(*first2, *first1))
-                return false;
-            else if (comp(*first1, *first2))
-                return true;
-            ++first1;
-            ++first2;
-        }
-        return (first2 != last2);
+        if (first2 == last2 || comp(*first2, *first1))
+            return false;
+        else if (comp(*first1, *first2))
+            return true;
+        ++first1;
+        ++first2;
     }
+    return (first2 != last2);
+}
+
+template <class InputIterator1, class InputIterator2>
+bool    equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+{
+    while (first1 != last1)
+    {
+        if (*first1 != *first2)
+            return false;
+        first1++;
+        first2++;
+    }
+    return true;
+}
+
+template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+bool    equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+{
+    while (first1 != last1)
+    {
+        if (pred(*first1, *first2) == false)
+            return false;
+        first1++;
+        first2++;
+    }
+    return true;
+}
 
 #endif
