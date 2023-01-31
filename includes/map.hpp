bc44ca1f82ed6e2c4d73e880c8a5ae61f829895e
Mohamed KARIM <karimkar999@gmail.com>
2023-01-31 06:20:59 +0100

    finish the most easiest container

--- includes/map.hpp
+++ includes/map.hpp
@@ -6,7 +6,7 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 11:05:33 by mkarim            #+#    #+#             */
-/*   Updated: 2023/01/09 13:07:34 by mkarim           ###   ########.fr       */
+/*   Updated: 2023/01/31 06:04:39 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,54 +16,114 @@
 # include <iostream>
 # include "../utils/red_black_tree.hpp"
 
-#define RED     0
-#define BLACK   1
-#define LEFT    3
-#define RIGHT   4
+namespace ft {
+    template <class Key,
+    class T,
+    class Compare = std::less<Key>,
+    class Allocator = std::allocator<std::pair<const Key, T> > >
+    class map
+    {
+        public:
+            typedef Key                                      key_type;
+            typedef T                                        mapped_type;
+            typedef std::pair<const key_type, mapped_type>        value_type;
+            typedef Compare                                  key_compare;
+            typedef Allocator                                allocator_type;
+            typedef typename allocator_type::reference       reference;
+            typedef typename allocator_type::const_reference const_reference;
+            typedef typename allocator_type::pointer         pointer;
+            typedef typename allocator_type::const_pointer   const_pointer;
+            typedef typename allocator_type::size_type       size_type;
+            typedef typename allocator_type::difference_type difference_type;
 
-// namespace ft {
-//     template <class Key,
-//     class T,
-//     class Compare = std::less<Key>,
-//     class Allocator = std::allocator<std::pair<const Key, T> >
-//     > class map
-//     {
-//         public:
-//             // types:
-//             typedef Key                                      key_type;
-//             typedef T                                        mapped_type;
-//             typedef std::pair<const key_type, mapped_type>        value_type;
-//             typedef Compare                                  key_compare;
-//             typedef Allocator                                allocator_type;
-//             typedef typename allocator_type::reference       reference;
-//             typedef typename allocator_type::const_reference const_reference;
-//             typedef typename allocator_type::pointer         pointer;
-//             typedef typename allocator_type::const_pointer   const_pointer;
-//             typedef typename allocator_type::size_type       size_type;
-//             typedef typename allocator_type::difference_type difference_type;
+        private:
+            RBT<Key, T> _tree;
+            size_type   _size;
 
-//             // typedef implementation-defined                   iterator;
-//             // typedef implementation-defined                   const_iterator;
-//             // typedef std::reverse_iterator<iterator>          reverse_iterator;
-//             // typedef std::reverse_iterator<const_iterator>    const_reverse_iterator;
+        public:
+            // explicit map(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type())
+            // {
+            //     _size = 0;
+            // }
+
+            // template <class InputIterator>
+            // map(InputIterator first, InputIterator last, const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type())
+            // {
+            //     while (first != last)
+            //     {
+            //         _tree.insert(first->first, first->second);
+            //         _size++;
+            //         first++;
+            //     }
+            // }
+
+            // map(const map& x)
+            // {
+            //     this->_size = x._size;
+            //     RBT<key, T>::iterator it = x.begin();
+
+            //     for (; it != x.end(); it++)
+            //     {
+            //         this->_tree.insert(it->first, it->second);
+            //         _size++;
+            //     }
+            // }
             
-//             map()
-//             {
-//                 RBT _tree;
-//             }
-            
-//             map(Key key, T value)
-//             {
-//                 RBT _tree(T, value);
-//             }
+            // void    clear()
+            // {
+            //     _tree.clear();
+            // }
 
-//             void    insert(Key key, T value)
-//             {
-//                 _tree.
-//             }
-//         private:
-//             RBT _tree;
-//     };
-// }
+            // size_type   count(const key_type& k) const
+            // {
+            //     RBT<key_type, mapped_type>::iterator it;
+            //     size_type                            count;
+
+            //     it = _tree.begin();
+            //     count = 0;
+
+            //     for (; it != _tree.end(); it++)
+            //     {
+            //         if (it->first == k)
+            //             count++;
+            //     }
+            //     return count;
+            // }
+
+            // bool    empty() const
+            // {
+            //     return (_size == 0);
+            // }
+
+            // iterator    end()
+            // {
+            //     return iterator(_tree.end());
+            // }
+
+            // const_iterator    end() const;
+            // {
+            //     return iterator(_tree.end());
+            // }
+
+            // iterator    find(const key_type& k)
+            // {
+                
+            // }
+
+            // const_iterator    find(const key_type& k) const
+            // {
+                
+            // }
+
+            // size_type   size() const
+            // {
+            //     return _size;
+            // }
+
+            // size_type
+
+            
+    };
+}
 
 #endif
---
+++ includes/stack.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 06:01:38 by mkarim            #+#    #+#             */
+/*   Updated: 2023/01/31 06:20:32 by mkarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vector.hpp"
+#include <stack>
+
+namespace ft
+{
+
+    template <class T, class Container = ft::vector<T> >
+    class stack
+    {
+        public:
+            typedef Container                                container_type;
+            typedef typename container_type::value_type      value_type;
+            typedef typename container_type::reference       reference;
+            typedef typename container_type::const_reference const_reference;
+            typedef typename container_type::size_type       size_type;
+
+        protected:
+            container_type c;
+
+        public:
+            bool    empty() const
+            {
+                return c.size() == 0;
+            }
+
+            void    pop()
+            {
+                c.pop_back();
+            }
+
+            void    push(const value_type& val)
+            {
+                c.push_back(val);
+            }
+
+            size_type   size() const
+            {
+                return c.size();
+            }
+
+            value_type& top()
+            {
+                return c.back();
+            }
+            
+            const   value_type& top() const
+            {
+                return c.back();
+            }
+    };
+}
