1c7f0ee810d7f81739fc83b62f85b3da633449fb
Mohamed KARIM <karimkar999@gmail.com>
2023-02-05 14:34:39 +0100

    coded all member function of set container, i have to finish non-member overloads now

--- includes/set.hpp
+++ includes/set.hpp
@@ -6,7 +6,7 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 10:30:43 by mkarim            #+#    #+#             */
-/*   Updated: 2023/02/03 11:03:46 by mkarim           ###   ########.fr       */
+/*   Updated: 2023/02/05 12:57:29 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,20 +64,46 @@ namespace ft {
 
 			set&	operator=(set& x)
 			{
+				this->clear();
 				_alloc = x._alloc;
 				_comp = x._comp;
-				
 				this->insert(x.begin(), x.end());
 				return *this;
 			}
 
-			// insert member functions
-			pair<iterator, bool>    insert(const value_type& val)
+			void	clear()
+			{
+				_tree.clear();
+			}
+
+			size_type   count(const value_type& k) const
+			{
+				ft::pair<iterator, bool> p = _tree.find(k);
+				if (p.second)
+					return 1;
+				return 0;
+			}
+
+			void	empty()
+			{
+				return size() = 0;
+			}
+
+			size_type	size() const
+			{
+				return _tree.size();
+			}
+
+			size_type	max_size() const
+			{
+				return _alloc.max_size();
+			}
+
+			pair<iterator, bool>	insert(const value_type& val)
 			{
 				pair<iterator, bool> it = _tree.insert(val);
 				return it;
 			}
-
 
 			iterator    insert(iterator position, const value_type& val)
 			{
@@ -96,6 +122,80 @@ namespace ft {
 					_tree.insert(*first);
 					first++;
 				}
+			}
+
+			iterator	find(const value_type& val) const
+			{
+				ft::pair<iterator, bool> p = _tree.find(val);
+				if (p.second)
+					return p.first;
+				return _tree.end();
+			}
+
+			void	erase(iterator position)
+			{
+				_tree.remove(*position);
+			}
+
+			size_type	erase(const value_type& val)
+			{
+				iterator it = find(val);
+				if (it == _tree.end())
+					return 0;
+				erase(it);
+				return 1;
+			}
+
+			void	erase(iterator first, iterator last)
+			{
+				ft::stack<key_type> st;
+				ft::stack<key_type> to_erase;
+				
+				while (first != last)
+				{
+					st.push(*first);
+					first++;
+				}
+
+				while (!st.empty())
+				{
+					to_erase.push(st.top());
+					st.pop();
+				}
+
+				while (!to_erase.empty())
+				{
+					erase(to_erase.top());
+					to_erase.pop();
+				}
+			}
+
+			key_compare	key_comp() const
+			{
+				return _comp;
+			}
+
+			value_compare	value_compare() const
+			{
+				return value_compare();
+			}
+
+			iterator	lower_bound(const value_type& k)
+			{
+				iterator it = find(k);
+				if (it != _tree.end())
+					return it;
+				return _tree.upper_bound(k);
+			}
+
+			iterator	upper_bound(const value_type& k)
+			{
+				return _tree.upper_bound(k);
+			}
+
+			pair<iterator, iterator>	equal_range(const key_type& k)
+			{
+				return pair<iterator, iterator>(lower_bound(k), upper_bound(k));
 			}
 
 			void    print()
