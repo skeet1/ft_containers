855309791efb70dd24ae1b85e9362953f1f96133
Mohamed KARIM <karimkar999@gmail.com>
2023-02-06 07:39:41 +0100

    finish iterator_traits

--- utils/iterator_traits.hpp
+++ utils/iterator_traits.hpp
@@ -6,16 +6,44 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 06:47:57 by mkarim            #+#    #+#             */
-/*   Updated: 2023/02/03 06:49:06 by mkarim           ###   ########.fr       */
+/*   Updated: 2023/02/06 06:38:29 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #ifndef ITERATOR_TRAITS_HPP
-// #define ITERATOR_TRAITS_HPP
+#ifndef ITERATOR_TRAITS_HPP
+#define ITERATOR_TRAITS_HPP
 
-// #include "red_black_tree.hpp"
+#include <iterator>
+#include "red_black_tree.hpp"
 
-// template <class Iterator>
-// class iterator_traits
+template <class Iterator>
+class iterator_traits
+{
+	typedef typename Iterator::differene_type       difference_type;
+	typedef typename Iterator::value_type           value_type;
+	typedef typename Iterator::pointer              pointer;
+	typedef typename Iterator::reference            reference;
+	typedef typename Iterator::iterator_category    iterator_category;
+};
 
-// #endif
+template <class T>
+class iterator_traits<T*>
+{
+	typedef ptrdiff_t difference_type;
+	typedef T value_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef std::random_access_iterator_tag iterator_category;
+};
+
+template <class T>
+class iterator_traits<const T*>
+{
+	typedef ptrdiff_t difference_type;
+	typedef T value_type;
+	typedef const T* pointer;
+	typedef const T& reference;
+	typedef std::random_access_iterator_tag iterator_category;
+};
+
+#endif
