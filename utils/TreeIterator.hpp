d16f28ad3db0915e4a905f461eb2edab997e8ca5
Mohamed KARIM <karimkar999@gmail.com>
2023-02-12 10:06:37 +0100

    done with const_iterator of the tree

--- utils/TreeIterator.hpp
+++ utils/TreeIterator.hpp
@@ -6,7 +6,7 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 18:01:30 by mkarim            #+#    #+#             */
-/*   Updated: 2023/02/08 18:28:56 by mkarim           ###   ########.fr       */
+/*   Updated: 2023/02/12 09:30:16 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,24 @@
 
 namespace ft
 {
-	template <class Category,			// iterator::iterator_category
-	class T,							// iterator::value_type
-	class Node,							// Node_type
-	class Distance = ptrdiff_t,			// iterator::difference_type
-	class Pointer = T*,					// iterator::pointer
-	class Reference = T&				// iterator::reference
-	>
-	class TreeIterator : public ft::iterator<std::bidirectional_iterator_tag, T>
+	template <class T, class Node, class Alloc>
+	class TreeIterator
     {
 		public:
-			typedef typename ft::iterator_traits<iterator<Category, T> >::value_type				value_type;
-			typedef typename ft::iterator_traits<iterator<Category, T> >::difference_type			difference_type;
-			typedef typename ft::iterator_traits<iterator<Category, T> >::pointer					pointer;
-			typedef typename ft::iterator_traits<iterator<Category, T> >::reference					reference;
-			typedef typename ft::iterator_traits<iterator<Category, T> >::iterator_category			iterator_tag;
+			typedef T											value_type;
+			typedef T*											pointer;
+			typedef T&											reference;
+			typedef Alloc										allocator_type;
+			typedef typename allocator_type::difference_type	difference_type;
+			typedef typename std::bidirectional_iterator_tag	iterator_tag;
 		
 		private:
 			Node*	curr_node;
 		public:
+			Node*	base() const
+			{
+				return curr_node;
+			}
 			TreeIterator() : curr_node(nullptr)
 			{
 			}
@@ -48,15 +47,15 @@ namespace ft
 			TreeIterator(Node* node) : curr_node(node)
 			{
 			}
-
-			value_type*    operator->()
+			
+			pointer    operator->()
 			{
-				return &curr_node->_val;
+				return  &(*(this->curr_node->_val));
 			}
 
-			value_type      operator*()
+			reference      operator*()
 			{
-				return  this->curr_node->_val;
+				return  *(this->curr_node->_val);
 			}
 
 			bool    operator==(const TreeIterator it)
@@ -71,6 +70,141 @@ namespace ft
 
 			TreeIterator    operator++()
 			{
+				if (!curr_node)
+					return TreeIterator(curr_node);
+				Node*   succ = curr_node->_right;
+
+				while (succ && succ->_left)
+					succ = succ->_left;
+				if (succ)
+					curr_node = succ;
+				else
+				{
+					while (curr_node && curr_node->_parent && curr_node == curr_node->_parent->_right)
+						curr_node = curr_node->_parent;
+					curr_node = curr_node->_parent;
+				}
+				return TreeIterator(curr_node);
+			}
+			
+			TreeIterator    operator++(int)
+			{
+				if (!curr_node)
+					return TreeIterator(curr_node);
+				Node*   save_curr_node = curr_node;
+				Node*   succ = curr_node->_right;
+
+				while (succ && succ->_left)
+					succ = succ->_left;
+				if (succ)
+					curr_node = succ;
+				else
+				{
+					while (curr_node && curr_node->_parent && curr_node == curr_node->_parent->_right)
+						curr_node = curr_node->_parent;
+					curr_node = curr_node->_parent;
+				}
+				return TreeIterator(save_curr_node);
+			}
+			
+			TreeIterator    operator--()
+			{
+				if (!curr_node)
+					return TreeIterator(curr_node);
+				Node*   predec = curr_node->_left;
+				
+				while (predec && predec->_right)
+					predec = predec->_right;
+				if (predec)
+					curr_node = predec;
+				else
+				{
+					while (curr_node && curr_node->_parent && curr_node == curr_node->_parent->_left)
+						curr_node = curr_node->_parent;
+					curr_node = curr_node->_parent;
+				}
+				return TreeIterator(curr_node);
+			}
+			
+			TreeIterator    operator--(int)
+			{
+				if (!curr_node)
+					return TreeIterator(curr_node);
+				Node*   save_curr_node = curr_node;
+				Node*   predec = curr_node->_left;
+				
+				while (predec && predec->_right)
+					predec = predec->_right;
+				if (predec)
+					curr_node = predec;
+				else
+				{
+					while (curr_node && curr_node->_parent && curr_node == curr_node->_parent->_left)
+						curr_node = curr_node->_parent;
+					curr_node = curr_node->_parent;
+				}
+				return TreeIterator(save_curr_node);
+			}
+    };
+template <class T, class Node, class Alloc>
+	class TreeConstIterator
+    {
+		public:
+			typedef T											value_type;
+			typedef T*											pointer;
+			typedef T&											reference;
+			typedef Alloc										allocator_type;
+			typedef typename allocator_type::difference_type	differene_type;
+			typedef typename std::bidirectional_iterator_tag	iterator_tag;
+		
+		private:
+			Node*	curr_node;
+		public:
+			Node*	base() const
+			{
+				return this->curr_node;
+			}
+			TreeConstIterator() : curr_node(nullptr)
+			{
+			}
+
+			template <class T1, class Node1, class Diff1> 
+			TreeConstIterator(const TreeIterator<T1, Node1, Diff1> &obj): curr_node(obj.base())
+			{
+			}
+
+			~TreeConstIterator()
+			{
+			}
+			
+			TreeConstIterator(Node* node) : curr_node(node)
+			{
+			}
+			
+			pointer    operator->() const
+			{
+				return  &(*(this->curr_node->_val));
+			}
+
+			reference      operator*() const
+			{
+				return  *(this->curr_node->_val);
+			}
+
+			bool    operator==(const TreeConstIterator it)
+			{
+				return this->curr_node == it.curr_node;
+			}
+
+			bool    operator!=(const TreeConstIterator it)
+			{
+				return this->curr_node != it.curr_node;
+			}
+
+			TreeConstIterator    operator++()
+			{
+				if (!curr_node)
+					return TreeConstIterator(curr_node);
 				Node*   succ = curr_node->_right;
 
 				while (succ && succ->_left)
@@ -86,8 +220,10 @@ namespace ft
 				return curr_node;
 			}
 			
-			TreeIterator    operator++(int)
+			TreeConstIterator    operator++(int)
 			{
+				if (!curr_node)
+					return TreeConstIterator(curr_node);
 				Node*   save_curr_node = curr_node;
 				Node*   succ = curr_node->_right;
 
@@ -104,8 +240,10 @@ namespace ft
 				return save_curr_node;
 			}
 			
-			TreeIterator    operator--()
+			TreeConstIterator    operator--()
 			{
+				if (!curr_node)
+					return TreeConstIterator(curr_node);
 				Node*   predec = curr_node->_left;
 				
 				while (predec && predec->_right)
@@ -121,8 +259,10 @@ namespace ft
 				return curr_node;
 			}
 			
-			TreeIterator    operator--(int)
+			TreeConstIterator    operator--(int)
 			{
+				if (!curr_node)
+					return TreeConstIterator(curr_node);
 				Node*   save_curr_node = curr_node;
 				Node*   predec = curr_node->_left;
 				
@@ -139,77 +279,6 @@ namespace ft
 				return save_curr_node;
 			}
     };
-	template <class Category,
-	class T,
-	class Node,
-	class Distance = ptrdiff_t,
-	class Pointer = T*,
-	class Reference = T&
-	>
-	class TreeRevIterator {
-		public:
-			typedef typename ft::iterator_traits<iterator<Category, T> >::value_type				value_type;
-			typedef typename ft::iterator_traits<iterator<Category, T> >::difference_type			difference_type;
-			typedef typename ft::iterator_traits<iterator<Category, T> >::pointer					pointer;
-			typedef typename ft::iterator_traits<iterator<Category, T> >::reference					reference;
-			typedef typename ft::iterator_traits<iterator<Category, T> >::iterator_category			iterator_tag;
-
-		private:
-			TreeIterator<Category, T, Node> _it;
-
-		public:
-			TreeRevIterator(): _it()
-			{
-			}
-
-			TreeRevIterator(TreeIterator<Category, T, Node> it) : _it(it)
-			{
-			}
-			
-			~TreeRevIterator()
-			{
-			}
-
-			TreeIterator<Category, T, Node>    operator->()
-			{
-				return _it;
-			}
-
-			value_type      operator*()
-			{
-				return  this->curr_node->_val;
-			}
-
-			bool    operator==(const TreeRevIterator it)
-			{
-				return this->_it == it._it;
-			}
-
-			bool    operator!=(const TreeRevIterator it)
-			{
-				return !(_it == it._it);
-			}
-
-			TreeRevIterator    operator++()
-			{
-				return TreeRevIterator(--_it);
-			}
-			
-			TreeRevIterator    operator++(int)
-			{
-				return TreeRevIterator(_it--);
-			}
-			
-			TreeRevIterator    operator--()
-			{
-				return TreeRevIterator(++_it);
-			}
-			
-			TreeRevIterator    operator--(int)
-			{
-				return TreeRevIterator(_it++);
-			}
-	};
 }
 
 #endif
